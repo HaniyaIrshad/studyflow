5d5ff3e063ecd8d52f3acccb4d381856b9444919
Haniya Irshad <haniya.tech.cs@gmail.com>
2026-05-13 19:05:29 +0630

    Implemented user signup system

--- users/views.py
+++ users/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 def signup(request):
-    return render(request, 'signup.html')
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'signup.html', {'form': form})
