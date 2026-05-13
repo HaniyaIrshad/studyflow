1b3c2852156d94d730cd1bd3f81f0b8349349fb1
Haniya Irshad <haniya.tech.cs@gmail.com>
2026-05-13 18:32:51 +0630

    Configured app-level URL routing

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from tasks.views import home
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('', include('tasks.urls')),
 ]
