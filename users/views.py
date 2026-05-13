f84e619702ef5dfbe139a317004a66a408234139
Haniya Irshad <haniya.tech.cs@gmail.com>
2026-05-13 19:10:56 +0630

    Implemented login system and navigation

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 
 def signup(request):
 
@@ -7,10 +9,36 @@ def signup(request):
         form = UserCreationForm(request.POST)
 
         if form.is_valid():
-            form.save()
+            user = form.save()
+
+            login(request, user)
+
             return redirect('home')
 
     else:
         form = UserCreationForm()
 
     return render(request, 'signup.html', {'form': form})
+
+
+def user_login(request):
+
+    if request.method == 'POST':
+
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})
