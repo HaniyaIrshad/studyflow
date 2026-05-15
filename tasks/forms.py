e7af0bde99942b6cf77fc603a97b65687bf2f669
Haniya Irshad <haniya.tech.cs@gmail.com>
2026-05-15 12:09:06 +0630

    Improved project structure and homepage

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,11 +1,13 @@
 from django import forms
-from .models import Subject,Task, Note
+from .models import Subject, Task, Note
+
 
 class SubjectForm(forms.ModelForm):
 
     class Meta:
         model = Subject
         fields = ['name']
+
 
 class TaskForm(forms.ModelForm):
 
@@ -14,13 +16,21 @@ class TaskForm(forms.ModelForm):
         fields = ['subject', 'title', 'due_date', 'priority']
 
         widgets = {
-            'due_date': forms.DateInput(attrs={'type': 'date'})
+
+            'due_date': forms.DateInput(
+                attrs={'type': 'date'}
+            ),
+
+            'title': forms.TextInput(
+                attrs={
+                    'placeholder': 'Enter task'
+                }
+            ),
         }
+
 
 class NoteForm(forms.ModelForm):
 
     class Meta:
-
         model = Note
-
         fields = ['subject', 'title', 'content']
